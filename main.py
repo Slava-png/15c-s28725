17a101fcb8f9a6c46863356219b8a19e63ec2f2a
s28725 <s28725@pjwstk.edu.pl>
2024-03-18 15:17:26 +0100

    Task 8: Inheritance

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from module import square_generator as sg
+from module import cubic_generator as cg
 
 sg = sg.SquareGenerator()
 sg.e_squares(1, 10)
+
+cg = cg.CubicGenerator()
+cg.e_squares(1, 10)
 
