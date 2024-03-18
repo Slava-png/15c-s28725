e6ed8a9f2c99d2947e2baa2c8c5427aed554f2ac
s28725 <s28725@pjwstk.edu.pl>
2024-03-18 14:38:45 +0100

    Task2: Functions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-# =================== FIRST TASK ===================
+# =================== SECOND TASK ===================
 
-comprehensions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-squares = [x ** 2 for x in comprehensions]
+def e_squares(start, end):
+    squares = [x ** 2 for x in range(start, end)]
 
-print("List of squares from 1 to 10: ", squares)
+    print("List of squares from 1 to 10: ", squares)
+
+e_squares(1, 15)
