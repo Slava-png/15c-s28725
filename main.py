7d1a732fc9dd59f0c0915ac728499a125ce78a00
s28725 <s28725@pjwstk.edu.pl>
2024-03-18 14:48:29 +0100

    Task 5: Exceptions

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import math
-# =================== THIRD TASK ===================
 
 
 class SquareGenerator:
     def e_squares(self, start, end):
+        if start > end:
+            raise ValueError("Start is greater than end")
+
         squares = [math.pow(x, 2) for x in range(start, end)]
 
         print("List of squares from 1 to 10: ", squares)
 
 
-SquareGenerator().e_squares(1, 10)
+SquareGenerator().e_squares(11, 10)
