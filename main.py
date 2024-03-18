d8b08546db0c02230ada7a3aa174c5af46703d7e
s28725 <s28725@pjwstk.edu.pl>
2024-03-18 14:45:24 +0100

    Task4: Libraries

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import math
 # =================== THIRD TASK ===================
+
 
 class SquareGenerator:
     def e_squares(self, start, end):
-        squares = [x ** 2 for x in range(start, end)]
+        squares = [math.pow(x, 2) for x in range(start, end)]
 
         print("List of squares from 1 to 10: ", squares)
 
